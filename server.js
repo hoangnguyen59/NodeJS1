72cdea3f5ae07f4e847e89785941a46326e748c5
hoangnguyen59 <54676756+hoangnguyen59@users.noreply.github.com>
2021-11-21 16:23:46 +0700

    lan 2

--- server.js
+++ server.js
@@ -24,7 +24,22 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:timestamp", (req,res) => {
+  let timestamp = req.params.timestamp;
+  if(timestamp.match(/\d{5,}/)){
+    timestamp = +timestamp;
+  }
+  let date = new Date(timestamp);
+  if(date.toUTCString() == "Invalid Date"){
+    res.json({error: "Invalid Date"})
+  }
+  res.json({unix: date.valueOf(), utc: date.toUTCString()})
+})
 
+app.get("/api/", (req,res) =>{
+  let date = new Date();
+  res.json({unix: date.valueOf(), utc: date.toUTCString()})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
